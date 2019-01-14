70b74a9526edc6578c0b69aa988439f37f8ef29c
wujiepeng <517987404@qq.com>
2019-01-14 22:22:42 +0800

    初始化

--- views/public/seekpass.php
+++ views/public/seekpass.php
@@ -11,52 +11,58 @@ $this->params['breadcrumbs'][] = $this->title;
 $this->registerMetaTag(['name' => 'keywords', 'content' => '']);
 $this->registerMetaTag(['name' => 'description', 'content' => ''], 'description');
 
+$this->registerCssFile('@web/public/wx/css/grid.css', ['depends' => 'app\assets\WxAsset']);
+$this->registerCssFile('@web/public/wx/css/normalize.css', ['depends' => 'app\assets\WxAsset']);
+$this->registerCssFile('@web/public/wx/css/common.css', ['depends' => 'app\assets\WxAsset']);
+$this->registerCssFile('@web/public/wx/css/register.css', ['depends' => 'app\assets\WxAsset']);
+
 # layer~~~start
-$this->registerJsFile('@web/public/kjd/js/layer/layer.js', ['depends' => ['app\assets\KjdAsset'], 'position' => View::POS_HEAD]);
+$this->registerJsFile('@web/public/wx/js/layer/layer.js', ['depends' => ['app\assets\WxAsset'], 'position' => View::POS_HEAD]);
 # layer~~~end
-?>
-<div class="gbox">
-    <div class="wraper2">
+?>	
+<div class="foget_line"></div>
+<div class="container_25 clearfix forget_content">
+    <h2>找回密码</h2>
+    <div class="forget_password">
         <?php
         $form = ActiveForm::begin([
                     'options' => ['id' => 'register_form'],
                     'fieldConfig' => [
                         'template' => "<li>{label}{input}{error}</li>",
-                        'labelOptions' => ['class' => false, 'for' => false],
-                        'errorOptions' => ['class' => 'warn'],
+                        'labelOptions' => ['class' => false, 'for' => false, 'style' => 'margin-right:10px;'],
+                        'errorOptions' => ['class' => 'exclamation'],
                     ]
         ]);
         ?>
-        <div class="register_box2">
-            <ul>
-                <?= $form->field($model, 'username', ['template' => "<li>{label}{input}{error}</li>", 'errorOptions' => ['class' => 'warn']])->textInput(['class' => 'reg_input reg_input_w forget_input', 'placeholder' => '请填写中国大陆手机号']); ?>
-                <?=
-                $form->field($model, 'verifyCode')->widget(Captcha::className(), [
-                    'template' => '{input}{image}',
-                    'captchaAction' => '/public/captcha',
-                    'options' => ['id' => 'verify', 'style' => 'float:left;', 'class' => 'reg_input reg_input_w2 forget_input', 'placeholder' => '请输入验证码'],
-                    'imageOptions' => ['alt' => '点击换图', 'title' => '点击换图', 'style' => 'float:right;margin-right:22px;border-radius:3px;']
-                ]);
-                ?>
-                <?= $form->field($model, 'short', ['template' => "<li>{label}{input}<input type=\"button\" class=\"getVerification_code\" value=\"获取验证码\" id=\"btnSend\">{error}</li>", 'errorOptions' => ['class' => 'warn']])->textInput(['id' => 'short', 'class' => 'reg_input reg_input_w2 forget_input', 'placeholder' => '请输入短信验证码']); ?>
-                <?= $form->field($model, 'password', ['template' => "<li>{label}{input}{error}</li>", 'errorOptions' => ['class' => 'warn']])->passwordInput(['class' => 'reg_input reg_input_w forget_input', 'placeholder' => '6-20位字母数字组合']); ?>
-                <?= $form->field($model, 'repass', ['template' => "<li>{label}{input}{error}</li>", 'errorOptions' => ['class' => 'warn']])->passwordInput(['class' => 'reg_input reg_input_w forget_input', 'placeholder' => '6-20位字母数字组合']); ?>
-            </ul>
-            <?= Html::submitButton('立即找回', ['class' => 'reg_button', 'id' => 'submit_button-btn']); ?>
-        </div>
+        <ul>
+            <?= $form->field($model, 'username')->textInput(['class' => 'forget_input', 'maxlength' => '11']); ?>
+            <?=
+            $form->field($model, 'verifyCode')->widget(Captcha::className(), [
+                'template' => '{input}{image}',
+                'captchaAction' => '/public/captcha',
+                'options' => ['id' => 'verify', 'class' => 'forget_input2 forget_input'],
+                'imageOptions' => ['alt' => '点击换图', 'title' => '点击换图', 'class' => 'code_img', 'style' => 'width:95px;height:28px;margin-left:10px;']
+            ]);
+            ?>
+            <?= $form->field($model, 'short', ['template' => "<li>{label}{input}<input type=\"button\" class=\"getVerification_code\" value=\"获取验证码\" id=\"btnSend\">{error}</li>", 'errorOptions' => ['class' => 'exclamation']])->textInput(['id' => 'short', 'class' => 'forget_input2 forget_input']); ?>
+            <?= $form->field($model, 'password')->passwordInput(['class' => 'forget_input']); ?>
+            <?= $form->field($model, 'repass')->passwordInput(['class' => 'forget_input']); ?>
+        </ul>
+        <?= Html::submitButton('立即找回', ['class' => 'forget_btn', 'id' => 'forget_btn']); ?>
         <?php ActiveForm::end(); ?>
     </div>
 </div>
+
 <script>
     //发送验证码
     $('#btnSend').on('click', function () {
         var phone = $("#member-username").val();
         if (phone == null || phone == "") {
-            layer.msg('请输入手机号码', {icon: 2, time: 1500});
+            layer.tips('请输入手机号码', '#member-username', {tips: [2, '#EA2000']});
             return false;
         }
         if (!(/^1(3|4|5|7|8)\d{9}$/.test(phone))) {
-            layer.msg('手机号码有误', {icon: 2, time: 1500});
+            layer.tips('手机号码有误', '#member-username', {tips: [2, '#EA2000']});
             return false;
         }
         if (!$(this).hasClass('not')) {
