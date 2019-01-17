a3496f9b67748c5dcd6ac6f1bdc7054b146c5994
wujiepeng <517987404@qq.com>
2019-01-17 17:39:45 +0800

    孵化模型

---
+++ models/IncubatorBase.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace app\models;
+
+use yii\db\ActiveRecord;
+use Yii;
+
+class IncubatorBase extends ActiveRecord
+{
+
+    public static function tableName()
+    {
+        return "{{%incubator_base}}";
+    }
+
+    public function attributeLabels()
+    {
+        return [
+            'name' => '主题名称',
+            'code' => '主题标识'
+        ];
+    }
+
+    public function rules()
+    {
+        return [
+                [['name', 'code'], 'required', 'message' => '{attribute}必填'],
+                ['code', 'match', 'pattern' => '/^[A-Za-z]+$/', 'message' => '请填写字母'],
+        ];
+    }
+
+    public function add($data)
+    {
+        if ($this->load($data) && $this->save())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static function getList($where = [])
+    {
+        $list = self::find()->where($where)->all();
+        return $list;
+    }
+
+}
